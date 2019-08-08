a77580f6f8f9e9b91bdfc8b78867b36b41e1e220
Thomas Maier <tmaier@mac_ebode.gingco-nm.de>
2019-08-08 15:03:09 +0200

    Initial Setup

---
+++ src/renderer/renderer.tsx
@@ -0,0 +1,7 @@
+/**
+ * React renderer.
+ */
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+ReactDOM.render(<div className="app">Hello Welt!</div>, document.getElementById('app'))
