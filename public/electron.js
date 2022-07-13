c5a427a219a6973c3ac9e7eec0d0b815b9238232
VWQL40T <thomas.maier1@volkswagen.de>
2022-07-13 13:13:55 +0200

    update

--- public/electron.js
+++ public/electron.js
@@ -16,9 +16,7 @@ function createWindow() {
 app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+  if (process.platform !== 'darwin') app.quit()
 })
 
 app.on('activate', () => {
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,43 @@
 import React, { FC } from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import * as serviceWorker from './serviceWorker'
+import { AuthProvider } from 'oidc-react'
 
 // Styles
 import './index.scss'
 
 // ==========================================================
 const App: FC = () => {
-  return <div />
+  const oidcConfig = {
+    onSignIn: async (user: any) => {
+      console.log('here')
+      alert('You just signed in, congratz! Check out the console!')
+      console.log(user)
+      window.location.hash = ''
+    },
+    authority: 'https://idp.cloud.vwgroup.com/auth/realms/kums-mfa',
+    clientId: 'idp-947b4569-2119-4899-9b58-aac5009f3de4',
+    responseType: 'code',
+    redirectUri: 'http://localhost:3000'
+  }
+
+  return (
+    <AuthProvider {...oidcConfig}>
+      <div className="App">
+        <header className="App-header">
+          <p>OIDC React</p>
+        </header>
+      </div>
+    </AuthProvider>
+  )
 }
 
 // ==========================================================
-ReactDOM.render(<App />, document.getElementById('root'))
+const rootElement = document.getElementById('root')
+if (rootElement) {
+  const root = createRoot(rootElement)
+  root.render(<App />)
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
